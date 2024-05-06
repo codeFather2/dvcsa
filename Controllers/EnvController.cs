13ba0e1c0aaa6387d1797efc8fccdea39550946f
alex-nightvision <alex@nightvision.net>
2024-05-06 11:52:43 -0400

    add fixes from meeting

--- Controllers/EnvController.cs
+++ Controllers/EnvController.cs
@@ -3,16 +3,24 @@ using Microsoft.AspNetCore.Mvc;
 namespace dvcsa.Controllers;
 
 [ApiController]
-[Route("/api/.env")]
+[Route("/.env")]
 public class EnvController : ControllerBase
 {
     [HttpGet(Name = "GetEnv")]
     public ActionResult<string> Get()
     {
        var envContent = """
-                      DB_NAME=dvcsa
-                      DB_USER=dvcsa
-                      DB_PASSWORD=dvcsa
+                      DB_NAME=crapi
+                      DB_USER=crapi
+                      DB_PASSWORD=crapi
+                      DB_HOST=postgresdb
+                      DB_PORT=5432
+                      SERVER_PORT=8080
+                      MONGO_DB_HOST=mongodb
+                      MONGO_DB_PORT=27017
+                      MONGO_DB_USER=crapi
+                      MONGO_DB_PASSWORD=crapi
+                      MONGO_DB_NAME=crapi
                       """;
         Response.Headers.Add("Content-Disposition", "attachment; filename=env");
         return envContent;
---
+++ check-endpoint.sh
@@ -0,0 +1,15 @@
+echo "==========================================="
+echo "SQL INJECTION"
+echo "curl \"http://127.0.0.1:8999/Users?name='%20OR%20'1'='1\""
+echo
+curl "http://127.0.0.1:8999/Users?name='%20OR%20'1'='1"
+echo 
+echo "==========================================="
+echo "Laravel - Sensitive Information Disclosure"
+echo 'curl http://127.0.0.1:8999/.env'
+echo
+curl http://127.0.0.1:8999/.env
+
+read
+open -a Google\ Chrome "http://127.0.0.1:8999/.env" &
+open -a Google\ Chrome "http://127.0.0.1:8999/users?name='%20OR%20'1'='1" &
