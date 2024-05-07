a86fdf2538b1a223cd7cf101e1d9599a0cb9646f
alex-nightvision <alex@nightvision.net>
2024-05-07 11:25:27 -0400

    fix chrome script

--- check-endpoint.sh
+++ check-endpoint.sh
@@ -18,4 +18,4 @@ echo
 read
 open -a Google\ Chrome "http://127.0.0.1:8999/.env" &
 open -a Google\ Chrome "http://127.0.0.1:8999/Users?name='%20OR%20'1'='1" &
-open -a Google\ Chrome "http://127.0.0.1:8999/Users/search?name=<script>alert()</script>"
+open -a Google\ Chrome "http://127.0.0.1:8999/Users/search?name=<script>alert()</script>" &
